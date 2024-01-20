9a3a84bd8206f65fcd5b56e399bcb7c73260f915
Danny Liu <dannyjialiliu@gmail.com>
2024-01-19 22:05:32 -0800

    add unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders membership cost savings calculator', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/membership cost savings calculator/i);
   expect(linkElement).toBeInTheDocument();
 });
